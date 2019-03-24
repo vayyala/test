a2144a31528d6286adaade73ca9aa4605ca3ce43
vayyala <vayyala08@gmail.com>
2019-03-24 19:37:02 +0000

    Add files via upload

--- codePost.py
+++ codePost.py
@@ -1,12 +1,14 @@
-api_key = ''
+# set API key via %env cp_api_key = <API KEY>
+api_key = %env cp_api_key
 course_name = 'New course'
 course_period = '2019'
-assn_name = 'NBody'
-student_name = 'richard@codepost.io'
+student_name = sys.argv[1]
+assn_name = sys.argv[2]
+
 import requests
 import codePost
-try:
-  assn = codePost.get_assignment_info_by_name(api_key, course_name, course_period, assn_name)
-  sub = codePost.get_assignment_submissions(api_key, assn.id, student=student_name)
-  f = codePost.post_file(api_key, sub.id, "test_output", String(grades), "txt")
-  print(f)'''
+assn = codePost.get_assignment_info_by_name(api_key, course_name, course_period, assn_name)
+sub = codePost.get_assignment_submissions(api_key, assn.id, student=student_name)
+f = codePost.post_file(api_key, sub.id, "test_output", String(test_output), "txt")
+print(f.id)
+codePost.post_comment(api_key, f, "Final grade", 10, 0, 1, 1, 1)
