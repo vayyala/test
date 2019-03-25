5a9ab8c2fc7bdfeab126e233c9a0d3c816e52eb4
vayyala <vayyala08@gmail.com>
2019-03-25 00:18:02 +0000

    updates

--- testing.py
+++ testing.py
@@ -1,22 +1,63 @@
+# Written for UCSB INT 5
+
 import os
 import sys
+# install by 'pip install codePost'. Documentation can be found at https://github.com/codepost-io/codePost-python
 import codePost
 
-def get_grade_script(student_email, assn_name):
+api_key = "ee1c6ca927c9fbb1532312d6636951f9fcc44bab" # set API key via %env cp_api_key = <API KEY>
+course_name = 'New course'
+course_period = '2019'
+
+def get_grade_snippet(student_email, assignment_name):
   return '''"import os\\n",
     "test_output = [ok.grade(q[:-3]) for q in os.listdir('tests') if q.startswith('q')]\\n",
-    "%run upload_tests.py {student_email} {assn_name} test_output\\n"'''.format(student_email=student_email, assn_name=assn_name)
+    "%run upload_tests.py {student_email} {assignment_name} test_output\\n"'''.format(student_email=student_email, assignment_name=assignment_name)
 
-def flatten(input_dir, output_dir):
-  oldF = open(input_dir, 'r')
-  newF = open(output_dir, 'w')
-  student_email=input_dir.split('_')[0]
-  assn_name=input_dir.split('_')[1].split('.')[0]
-#   upload file to codePost
-  for line in oldF:
-    newF.write(line.replace("#_ = ok.submit()", get_grade_script(student_email, assn_name)).replace("\"\"", "\""))
-  oldF.close()
-  newF.close()
+from shutil import copyfile
+
+def flatten(input_dir, output_dir, assignment):
+    ## if output directory doesnt exist then create it
+    if(not os.path.exists(output_dir)):
+        os.mkdir(output_dir)
+    if(not os.path.exists(output_dir+"_temp")):
+        os.mkdir(output_dir+"_temp")
+    temp_dir = output_dir + "_temp"
+    for file in os.listdir(input_dir):
+        ## Check for only ipynb
+        print(file)
+        if(file.endswith(".ipynb")):
+            copyfile(input_dir+'/'+file, temp_dir+'/'+file)
+            oldF = open(temp_dir+'/'+file, 'r')
+            newF = open(output_dir+'/'+file, 'w')
+            student_email=file.split('_')[0]
+            for line in oldF:
+                #newF.write(line.replace('_ = ok.auth(inline=True)', '#_ = ok.auth(inline=True)').replace("_ = ok.submit()", "#_ = ok.submit()").replace("4 = 2 + 2", "# 4 = 2 + 2").replace("six = two plus two", "# six = two plus two" ))
+                newF.write(line.replace("#_ = ok.submit()", get_grade_snippet(student_email, assignment["name"])).replace("\"\"", "\""))
+            oldF.close()
+            newF.close()
+
+            # upload to codePost
+            upload_notebook(file, open(temp_dir+'/'+file, 'r').read(), student_email, assignment)
+
+            os.remove(temp_dir+'/'+file)
+            # print(student_dir+'_'+file)
+    os.rmdir(output_dir + "_temp")
+    # pass
+
+def upload_notebook(file_name, file_code, student_email, assignment):
+    print(student_email)
+    file_to_upload = {"name": file_name, "code": file_code, "extension": "ipynb"}
+    # syntax: upload_submission(api_key, assignment, students, files, mode=DEFAULT_UPLOAD_MODE)
+    result = codePost.upload_submission(api_key, assignment, [student_email], [file_to_upload], codePost.UploadModes.OVERWRITE)
+    if (result):
+        print("Successfully uploaded notebook for %s" % student_email)
 
 if __name__ == "__main__":
-  flatten(sys.argv[1],sys.argv[2])
+    assignment = codePost.get_assignment_info_by_name(api_key, course_name, course_period, sys.argv[3])
+    if(not assignment):
+        print(assignment)
+        raise Exception("No Assignment found with the given name and course info. Please check to make sure the name is correct.")
+
+    # flatten('./ucsb-int5-fa18-hw01','./ucsb-int5-fa18-hw01_flatten')
+    flatten(sys.argv[1],sys.argv[2], assignment)
--- upload_tests.py
+++ upload_tests.py
@@ -1,20 +1,52 @@
-# set API key via %env cp_api_key = <API KEY>
 import os
 import sys
 import requests
+# install by 'pip install codePost'. Documentation can be found at https://github.com/codepost-io/codePost-python
 import codePost
 
-api_key = os.environ["cp_api_key"]
+api_key = os.environ["cp_api_key"] # set API key via %env cp_api_key = <API KEY>
 course_name = 'New course'
 course_period = '2019'
-student_name = sys.argv[1]
+student_email = sys.argv[1]
 assn_name = sys.argv[2]
 test_output = sys.argv[3]
 
-assn = codePost.get_assignment_info_by_name(api_key, course_name, course_period, assn_name)
-print(assn)
-sub = codePost.get_assignment_submissions(api_key, assn['id'], student=student_name)
-print(sub)
-f = codePost.post_file(api_key, sub[0]['id'], "test_output", str(test_output), "txt")
-print(f['id'])
-codePost.post_comment(api_key, f, "Final grade", 10, 1, 2, 1, 1)
+upload_test_output(api_key, course_name, course_period, student_name, assn_name, test_output)
+
+def upload_test_output(api_key, course_name, course_period, student_name, assn_name, test_output):
+    # Find Assignment
+    assn = codePost.get_assignment_info_by_name(api_key, course_name, course_period, assn_name)
+    if(not assn):
+        raise Exception("No Assignment found with the given name and course info. Please check to make sure the names are correct.")
+
+    # Find Submission for student
+    sub = codePost.get_assignment_submissions(api_key, assn['id'], student=student_email)
+    if(len(sub) != 1):
+        raise Exception("No submission found for this student and assignment.")
+
+    # Parse the test output to the format desired to upload to codePost
+    test_output_to_be_uploaded = parse_test_output(test_output)
+
+    # Upload test output to codePost
+    new_file = codePost.post_file(api_key, sub[0]['id'], "test_output", str(test_output_to_be_uploaded), "txt")
+
+    # Add comments to file
+    add_comments(api_key, test_output, new_file)
+
+def parse_test_output(test_output):
+    """
+    Given a test output file and returns the file to be uploaded to codePost
+    NOTE: THIS FUNCTION SHOULD BE EDITED BASED ON DESIRED USER BEHAVIOR
+    """
+    # example: returns full test output
+    return test_output
+
+def add_comments(api_key, test_output, file):
+    """
+    Adds comments to a codePost file, given an API Key, output to parse for comments, and a file object
+    NOTE: THIS FUNCTION SHOULD BE EDITED BASED ON DESIRED USER BEHAVIOR
+    """
+    # example: posts a single comment
+    # syntax: post_comment(api_key, file, text, pointDelta, startChar, endChar, startLine, endLine, rubricComment=None)
+    # pointDelta is parsed as a negative. e.g., a pointDelta of 1 is -1 on codePost
+    codePost.post_comment(api_key, f, "Final grade", 10, 1, 2, 1, 1)
